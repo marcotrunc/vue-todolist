3ddaddeac5429c34fde85dfbc282baa6e113f848
marcotrunc <marcotrunc@gmail.com>
2022-01-28 14:55:28 +0100

    added the cycle that allows me to print new li

--- js/script.js
+++ js/script.js
@@ -4,10 +4,10 @@ Vue.config.devtools = true;
 const root = new Vue({
     el: '#root',
     data: {
-        thingsToDO: [
+        tasksToDO: [
             { text: 'tagliare i capelli', done: true, },
-            { text: 'tagliare i capelli', done: true, },
-            { text: 'tagliare i capelli', done: true, }
+            { text: 'tagliare il prato', done: true, },
+            { text: 'fare la spesa', done: true, }
         ],
     },
 });

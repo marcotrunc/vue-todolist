5defab88993ee7fde703ed78b95990b994f9691e
marcotrunc <marcotrunc@gmail.com>
2022-01-28 15:53:35 +0100

    added a method to insert a new task

--- js/script.js
+++ js/script.js
@@ -4,15 +4,24 @@ Vue.config.devtools = true;
 const root = new Vue({
     el: '#root',
     data: {
+        newTask: '',
         tasksToDO: [
-            { text: 'tagliare i capelli', done: true, },
-            { text: 'tagliare il prato', done: true, },
-            { text: 'fare la spesa', done: true, }
+            { text: 'tagliare i capelli', done: false, },
+            { text: 'tagliare il prato', done: false, },
+            { text: 'fare la spesa', done: false, },
         ],
     },
     methods: {
         cancelObject(index) {
             this.tasksToDO = this.tasksToDO.filter((item, i) => index !== i);
+        },
+        addNewTask() {
+            if (this.newTask) {
+                const newTask = this.newTask.trim()
+                const newObject = { text: newTask, done: false };
+                this.tasksToDO = [...this.tasksToDO, newObject];
+            }
+            this.newTask = '';
         },
     },
 });

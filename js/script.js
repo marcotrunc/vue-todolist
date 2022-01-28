912bed4ac5cddf81aa86abb382e19fe6c547e65a
marcotrunc <marcotrunc@gmail.com>
2022-01-28 17:01:24 +0100

    added a method to reverse the .done

--- js/script.js
+++ js/script.js
@@ -5,10 +5,11 @@ const root = new Vue({
     el: '#root',
     data: {
         newTask: '',
+        isDone: 'false',
         tasksToDO: [
             { text: 'tagliare i capelli', done: false, },
             { text: 'tagliare il prato', done: false, },
-            { text: 'fare la spesa', done: false, },
+            { text: 'fare la spesa', done: true, },
         ],
     },
     methods: {
@@ -23,5 +24,11 @@ const root = new Vue({
             }
             this.newTask = '';
         },
+        markAsDoneOrNot(index) {
+            this.tasksToDO.map((item, i) => {
+                if (index === i) this.tasksToDO[i].done = !this.tasksToDO[i].done;
+            });
+        }
+
     },
 });
